827ca5ac4ff16cff5f98c5f0a54736a7760c82c1
haquemofiz <m.haquemizu12@gmail.com>
2026-04-03 17:23:26 +0600

    Add api.js file with axios configuration and API methods

---
+++ frontend/src/services/api.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// Add token to requests
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const authAPI = {
+  register: (data) => api.post('/auth/register', data),
+  login: (data) => api.post('/auth/login', data)
+};
+
+export const postsAPI = {
+  getAll: () => api.get('/posts'),
+  getById: (id) => api.get(`/posts/${id}`),
+  create: (data) => api.post('/posts', data),
+  update: (id, data) => api.put(`/posts/${id}`, data),
+  delete: (id) => api.delete(`/posts/${id}`),
+  like: (id) => api.post(`/posts/${id}/like`),
+  addComment: (id, data) => api.post(`/posts/${id}/comments`, data)
+};
+
+export default api;
